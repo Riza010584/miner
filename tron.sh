f8feb22e5926699ef33567cc8321ffc27ab26b2a
Riza010584 <94797478+Riza010584@users.noreply.github.com>
2021-11-29 00:13:56 +0700

    Create tron.sh

---
+++ tron.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.36a/lolMiner_v1.36a.tar.gz
+tar -xvf lolMiner_v1.36a.tar.gz
+./1.36a/lolMiner --algo ETHASH --pool ethash-asia.unmineable.com:3333 --user TRX:TJ7jymLvWWaMBnzvUdRsQR47ueUyASQpAZ.bitbucket1 -p x --cpu 4
